75af5ef468061874b25fe277e7da20dd55345389
Sholana1 <shodunkedavid16@gmail.com>
2024-09-05 13:58:41 +0100

    Villa CRUD

---
+++ WhiteLagoonWeb/Controllers/VillaController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoonWeb.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var villa = _context.Villas.ToList();
+            return View(villa);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if (ModelState.IsValid) {
+                _context.Villas.Add(villa);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+            return View();
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? villa = _context.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (villa == null) { 
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(villa);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Villa villa)
+        {
+            if (!ModelState.IsValid && villa.Id >0)
+            {
+                _context.Villas.Update(villa);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Delete(int villaId) { 
+            Villa? villa = _context.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (villa == null) {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(villa);
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa villa)
+        {
+            Villa? objFromDb = _context.Villas.FirstOrDefault(u => u.Id == villa.Id);
+            if (objFromDb is not null) { 
+                _context.Villas.Remove(objFromDb);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
